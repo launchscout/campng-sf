8b8e05d15e349b80567f0f859fb171417460bc41
Kevin Rockwood <rockwood@rockmac.local>
2013-11-13 17:50:53 -0500

    Complete Lab 4

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,4 +4,9 @@
 
 angular.module("cookbook").controller("RecipesCtrl", function($scope, Recipe) {
   $scope.recipes = Recipe.all()
+  $scope.currentRecipe = null
+
+  $scope.showRecipe = function(recipe){
+    $scope.currentRecipe = recipe;
+  }
 });
