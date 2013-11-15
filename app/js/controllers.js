b0a89179f3b6f8ce94db9ce9f8bbde09ca2cc538
Kevin Rockwood <kevin@rockwood.me>
2013-11-15 17:05:57 -0500

    Complete lab7

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -9,3 +9,13 @@ angular.module("cookbook").controller("RecipesCtrl", function($scope, Recipe) {
 angular.module("cookbook").controller("RecipeDetailCtrl", function($scope, $routeParams, Recipe) {
   $scope.currentRecipe = Recipe.find({id: $routeParams.recipeId })
 });
+
+angular.module("cookbook").controller("RecipeNewCtrl", function($scope, $location, Recipe) {
+  $scope.currentRecipe = new Recipe()
+
+  $scope.submit = function(){
+    Recipe.create($scope.currentRecipe).then(function(){
+      $location.path( "/recipes" );
+    });
+  }
+});
--- app/js/routes.js
+++ app/js/routes.js
@@ -3,6 +3,11 @@
 //////////////////////////
 
 angular.module("cookbook").config(function($routeProvider) {
+  $routeProvider.when('/recipes/new', {
+    template: $('#recipe-new-template').html(),
+    controller: 'RecipeNewCtrl'
+  });
+
   $routeProvider.when('/recipes/:recipeId', {
     template: $('#recipe-detail-template').html(),
     controller: 'RecipeDetailCtrl'
